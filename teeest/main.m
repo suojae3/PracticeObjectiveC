ad3bad852461d02c227185fa7f45d13730e626bd
suojae3 <suojae3@gmail.com>
2024-03-10 21:07:49 +0900

    getter setter 분리 선언

--- teeest/main.m
+++ teeest/main.m
@@ -14,12 +14,13 @@ SomeClass *object = [[SomeClass alloc] init];
  */
 
 @interface Vehicle : NSObject {
-    int wheels2;
-    int seats2;
+    int wheels;
+    int seats;
 }
 
 //member method -> GetterSetter를 자동적으로 만들어주어 노가다성 코딩 방지
-@property int wheels;
+// 프로퍼티 선언 때 아래와 같이 getter setter 구분 가능
+@property (getter = getWheels, setter = wheels:) int wheels;
 @property int seats;
 
 //-(void)setWheels: (int)w;
@@ -30,26 +31,26 @@ SomeClass *object = [[SomeClass alloc] init];
 @end
 
 @implementation Vehicle
-@synthesize  wheels;
+@synthesize wheels;
 @synthesize seats;
 
--(void)setWheels: (int)w {
-    wheels2 = w;
-}
--(void)setSeats: (int)s {
-    seats2 = s;
-}
+//-(void)setWheels: (int)w {
+//    wheels= w;
+//}
+//-(void)setSeats: (int)s {
+//    seats= s;
+//}
 
 -(int)wheels {
-    return wheels2;
+    return wheels;
 }
 
 -(int)seats {
-    return seats2;
-    
+    return seats;
 }
+
 -(void)print {
-    NSLog(@"wheels: %i, seats: %i", wheels2, seats2);
+    NSLog(@"wheels: %i, seats: %i", wheels, seats);
 }
 @end
 
@@ -57,13 +58,17 @@ SomeClass *object = [[SomeClass alloc] init];
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
         //Vehicle *hello = [Vehicle new];
         //new -> 관례적으로 안씀 보통 alloc(동적할당) init(초기화) 메서드 체이닝으로 자주씀
-        Vehicle *hello = [[Vehicle alloc]init]; // Create Instance Object
+        Vehicle *hello = [[Vehicle alloc] init]; // Create Instance Object
         
         //[Receiver Message]
-        [hello setWheels:5];
+        
+        [hello wheels:5];
         [hello setSeats:3];
+        hello.wheels = 4;
+        hello.seats = 2;
         
         //[hello print];
         NSLog(@"wheels: %i, seats: %i", [hello wheels],[hello seats]);
